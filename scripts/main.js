88c3703a184307bd96434fe0858e454c5f531d75
filon13p <filon13p@gmail.com>
2020-11-25 13:09:50 +0300

    Print version

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+var cssStyle = document.getElementById('style');
+
+window.onload = function(){
+    if(localStorage && localStorage.getItem("style"))
+        cssStyle.href = localStorage.getItem("style");
+};
+
+function setStyle(newStyle){
+    cssStyle.href = newStyle;
+
+    if(localStorage)
+        localStorage.setItem("style", newStyle);
+};
