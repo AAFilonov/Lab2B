a48b2ab56f1a07e8737bf8518c7680c6923223e6
filon13p <filon13p@gmail.com>
2020-11-27 16:22:59 +0300

    много я сказал

---
+++ scripts/filters.js
@@ -0,0 +1,21 @@
+const filter_content = document.querySelectorAll('.content-2  .content-item')
+
+
+function update_list(){
+    console.log('лист обновлен');
+}
+
+function filterChangeHande(event) {
+
+    if (event.target.checked) {
+      
+        console.log(event.target.value+' выбран')
+	}
+	else {
+		console.log(event.target.value+' не выбран')
+	}
+}
+
+
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,35 @@
 var cssStyle = document.getElementById('style');
 
-window.onload = function(){
-    if(localStorage && localStorage.getItem("style"))
+window.onload = function () {
+    if (localStorage && localStorage.getItem("style"))
         cssStyle.href = localStorage.getItem("style");
 };
 
-function setStyle(newStyle){
+function setStyle(newStyle) {
     cssStyle.href = newStyle;
 
-    if(localStorage)
+    if (localStorage)
         localStorage.setItem("style", newStyle);
 };
+
+var inputs = document.getElementsByTagName('input');
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('click', filterChangeHande, false);
+}
+
+
+
+$(document).ready(function () {
+    $('#filters input').change(function () { // событие на изменение чекбокса
+        $('article').each(function (index, elem) { // перебрать все элементы
+            $(elem).show(); // включаем отображение элемента
+            let data = $(elem).data(); // получаем все data-атрибуты элемента
+            for (let prop in data) { // перебрать все характеристики элемента
+                if ($("#filters input[name='" + prop + "']:checkbox:checked").val() != undefined && // есть установленные чекбоксы
+                    $("#filters input[name='" + prop + "'][value='" + data[prop] + "']:checkbox:checked").val() == undefined) { // нет установленного чекбокса как атрибут элемента
+                    $(elem).hide(); // выключаем отображение элемента
+                }
+            }
+        });
+    });
+});
